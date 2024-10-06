489031ba29f639a5a3424a4cc977673eaac1f72c
killahBoo <edithlavender@LIVE.SE>
2024-10-06 14:54:48 +0200

    Kinda works okey

---
+++ ShitChatApp/ShitChatApp.Client/DTOs/Token.cs
@@ -0,0 +1,7 @@
+﻿namespace ShitChatApp.Client.DTOs
+{
+	public class Token
+	{
+		public string JWTtoken { get; set; } = string.Empty;
+    }
+}
--- ShitChatApp/ShitChatApp/Hubs/ChatHub.cs
+++ ShitChatApp/ShitChatApp/Hubs/ChatHub.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using ShitChatApp.Data;
 using ShitChatApp.Shared.Entities;
 
 namespace ShitChatApp.Hubs
 {
+	[Authorize]
 	public class ChatHub : Hub
 	{
-		private List<ChatRoom> roomList = new();
+		private static List<ChatRoom> roomList = new();
+		private User User = new();
 		private readonly RoomRepo _roomRepo;
 
 		public ChatHub(RoomRepo repo)
@@ -18,26 +22,33 @@ namespace ShitChatApp.Hubs
 		{
 			roomList = await _roomRepo.GetRooms();
 			Console.WriteLine("Rooms: " + roomList.Count);
-			await Clients.Caller.SendAsync("GetRooms", roomList);
+			User = await GetUser();
+			await Clients.Caller.SendAsync("GetRooms", roomList, User);
 		}
 
-		public async Task SendMessage(string user, string message)
+		public async Task SendMessage(ChatRoom room, User user, string message)
 		{
-			await Clients.All.SendAsync("RecieveMessage", user, message);
+			//skapar nytt meddelande och sparar i databasen
+			var newMessage = new Message(room.ChatRoomID, user.UserID, message, DateTime.UtcNow);
+			await _roomRepo.SaveMessage(newMessage);
+			newMessage.User = user;
+			await Clients.Group(room.ChatRoomID).SendAsync("RecieveMessage", newMessage);
+			//await Clients.All.SendAsync("RecieveMessage", user, message);
+			
 		}
 
-		//public async Task GetRooms()
-		//{
-		//	//hämtar alla rum när man kommer till CreateRoom.razor
-		//	roomList = await _roomRepo.GetRooms();
-		//	await Clients.Caller.SendAsync("GetRooms", roomList);
-		//}
+		public async Task StartChat()
+		{
+
+		}
 
 		public async Task<ChatRoom> CreateNewRoom(string roomName, int roomCode)
 		{
 			//skapar och lägger till nytt rum i databasen
 			var roomId = Guid.NewGuid().ToString();
+			var user = await GetUser();
 			var newRoom = new ChatRoom(roomId, roomName, roomCode);
+			newRoom.Users.Add(user);
 			await _roomRepo.CreateRoom(newRoom);
 			roomList = await _roomRepo.GetRooms();
 
@@ -49,11 +60,27 @@ namespace ShitChatApp.Hubs
 			return newRoom;
 		}
 
-		public async Task<ChatRoom> JoinRoom(string roomId)
+		public async Task<ChatRoom> JoinRoom(ChatRoom reqRoom)
 		{
-			var room = await _roomRepo.FindRoom(roomId);
-			await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
-			return room;
+			//var room = await _roomRepo.FindRoom(roomId);
+			var user = await GetUser();
+			var room = roomList.SingleOrDefault(r => r.ChatRoomID == reqRoom.ChatRoomID);
+			if (room is not null)
+			{
+				room.Users.Add(user);
+				Console.WriteLine("In hub, users in room: " + room.Users.Count);
+				await Groups.AddToGroupAsync(Context.ConnectionId, room.ChatRoomID);
+				await Clients.Group(room.ChatRoomID).SendAsync("UserJoined", user);
+				return room;
+			}
+			return null;
+		}
+
+		public async Task<User> GetUser()
+		{
+			var name = Context.User?.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Name)!.Value;
+			var user = await _roomRepo.GetUser(name);
+			return user;
 		}
 	}
 }
